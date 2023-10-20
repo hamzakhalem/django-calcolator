ef2f6bcc65f2e50f1e7e615d777ab16d0f2b383b
hamza <hamzakhalem97@gmail.com>
2023-10-20 21:57:37 +0100

    first commit

--- calcapp/views.py
+++ calcapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def submitquery(request):
+    q = request.GET['query']
+    return HttpResponse(q)
